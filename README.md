e1f3230d02724f7f1e851e0bb36e534d283c9d67
Eric Smith <eric@esmithy.net>
2014-01-06 22:26:52 -0700

    Built out the whole UI and hooked up a bunch of bindings that mostly seem to work.

--- README.md
+++ README.md
@@ -6,6 +6,19 @@ Classic ciphers implemented in JavaScript
 
 Where was I?
 
+I think this is very close -- the output isn't updating when the other model
+properties change, though. Googling for how to bind to a function result is
+strangely ineffective.
+
+
+So, how does one build a nice looking UI with Angular without being a graphic
+designer?
+
+Maybe with one of these?
+http://angular-ui.github.io/
+
+
+
 http://docs.angularjs.org/tutorial/step_02
 
 Trying to go through the tutorial. My version isn't working. If I add
--- client/controllers.js
+++ client/controllers.js
@@ -4,14 +4,23 @@
 var cipherApp = angular.module('cipherApp', []);
 
 
-cipherApp.controller('AlgorithmListCtrl',
+cipherApp.controller('Controller',
     function ($scope) {
-        $scope.hello = 'Hello';
         $scope.algorithms = [
             new Caesar(),
             new Playfair(),
             new RailFence(),
             new Vigenere()
         ]
+        $scope.algorithm = $scope.algorithms[0];
+        $scope.key = "";
+        $scope.direction = "encipher";
+        $scope.input = "the input";
+        $scope.output = function() {
+            if ($scope.direction === 'encipher') {
+                return $scope.algorithm.encipher($scope.input, $scope.key);
+            }
+            return $scope.algorithm.decipher($scope.input, $scope.key);
+        };
     }
 );
