e7c5666e09b004b3745b16066b09425cb43d8527
Eric Smith <eric@esmithy.net>
2014-01-08 22:34:57 -0700

    Working, I think. BIG RED HERRING trying to combine the watch statements into one single watch.

--- client/controllers.js
+++ client/controllers.js
@@ -1,26 +1,33 @@
-'use strict';
-
-//var cipherControllers = angular.module('cipherControllers', []);
 var cipherApp = angular.module('cipherApp', []);
-
 
 cipherApp.controller('Controller',
     function ($scope) {
+        'use strict';
         $scope.algorithms = [
             new Caesar(),
             new Playfair(),
             new RailFence(),
             new Vigenere()
-        ]
+        ];
         $scope.algorithm = $scope.algorithms[0];
         $scope.key = "";
         $scope.direction = "encipher";
-        $scope.input = "the input";
-        $scope.output = function() {
+        $scope.input = "";
+        $scope.output = "";
+
+        var getOutput = function () {
             if ($scope.direction === 'encipher') {
                 return $scope.algorithm.encipher($scope.input, $scope.key);
             }
             return $scope.algorithm.decipher($scope.input, $scope.key);
         };
-    }
-);
+
+        var updateOutput = function () {
+            $scope.output = getOutput();
+        };
+
+        $scope.$watch('algorithm', updateOutput);
+        $scope.$watch('key',       updateOutput);
+        $scope.$watch('direction', updateOutput);
+        $scope.$watch('input',     updateOutput);
+    });
